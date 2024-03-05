e928a18157d097bac59fe764470d8162d35b454e
Vadher Divyesh Maheshbhai <102305908+divyeshvadher@users.noreply.github.com>
2024-03-06 00:30:56 +0530

    Update forms.py

--- society_era/portal/forms.py
+++ society_era/portal/forms.py
@@ -22,14 +22,6 @@ class ReferenceId(forms.ModelForm):
         model = reference_id
         fields = ["user", "contact", "address",]
         labels = {"user": "Name", "contact": "Contact", "address": "Address",} 
-        
-    # def save(self, commit=True):
-    #     user = User.objects.create(username=self.cleaned_data['full_name'])
-    #     reference_id_instance = super(ReferenceId, self).save(commit=False)
-    #     reference_id_instance.user = user
-    #     if commit:
-    #         reference_id_instance.save()
-    #     return reference_id_instance
     
 class CommunityForm(forms.ModelForm):
     class Meta:
