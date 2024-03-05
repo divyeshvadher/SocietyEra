1d70fa7de88142d97cccb0580a98d3540f8fbcb1
Vadher Divyesh Maheshbhai <102305908+divyeshvadher@users.noreply.github.com>
2024-03-06 00:32:21 +0530

    Update urls.py

--- society_era/portal/urls.py
+++ society_era/portal/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("home/",views.HomePage, name='home'),
     path("community/",views.community, name='community'),
     path('build_community/', views.build_community, name='build_community'),
+    path('join_community/',views.join_community, name='join_community'),
     path("references/",views.references, name='references'),
     path("generate/",views.GenerateRefID, name='generate'),
     path("services/",views.services, name='services'),
